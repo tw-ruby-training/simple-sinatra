34be57cb800848ef0ae0244aa8faf9a14f58c96c
Niu Yameng <yameng.niu@rea-group.com>
2015-05-17 01:21:04 +1000

    add user registration and login

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'sinatra', '~> 1.4.6'
 gem 'sinatra-activerecord', '~> 2.0.6'
 gem "sqlite3"
 gem "rake"
+gem "haml"
--- app.rb
+++ app.rb
@@ -1,14 +1,40 @@
 require 'sinatra/base'
 require "sinatra/activerecord"
 
+class User < ActiveRecord::Base
+  validates_presence_of :username
+end
+
 class Myapp < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
   set :database, {adapter: "sqlite3", database: "myapp.sqlite3"}
 
   get '/' do
-    "hello"
+    haml :index
   end
-  
+
+  post '/create' do
+    if params[:username].empty? || params[:password].empty?
+      "Username or password can't be empty"
+    else
+      User.create(username: params[:username], password: params[:password])
+      "hello #{params[:username]}"
+    end
+  end
+
+  get '/login' do
+    haml :login
+  end
+
+  post '/session' do
+    user = User.where(username: params[:username], password: params[:password]).first
+    if user.nil?
+      "User not exists"
+    else
+      "Welcome #{user.username}"
+    end
+  end
+
   run! if app_file == $0
 end
